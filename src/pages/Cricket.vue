4eaed8474db7467b554e115c26c36251ce044d9e
Adrian Osmond <a.c.osmond@gmail.com>
2021-04-30 14:32:31 +0200

    Add more to cricket scorecard

--- src/pages/Cricket.vue
+++ src/pages/Cricket.vue
@@ -3,30 +3,46 @@
   <h2 class="text-xl font-semibold">
     Scorecard
   </h2>
-  <table class="table-auto font-mono text-gray-400">
-    <tr>
-      <td class="pr-6 py-2">
-        NC Dexter
-      </td>
-      <td class="px-12 py-2">
-        not out
-      </td>
-      <td class="pl-6 py-2 text-right">
-        13
-      </td>
-    </tr>
-    <tr>
-      <td class="pr-6 py-2">
-        DCI Endeavour
-      </td>
-      <td class="px-12 py-2">
-        not out
-      </td>
-      <td class="pl-6 py-2 text-right">
-        6
-      </td>
-    </tr>
+  <table class="table-auto mt-6">
+    <thead>
+      <tr>
+        <th
+          class="text-left font-semibold"
+          colspan="2"
+        >
+          Batsman
+        </th>
+        <th class="text-right w-12 font-semibold">
+          Runs
+        </th>
+      </tr>
+    </thead>
+    <tbody class="text-gray-400 font-mono">
+      <tr>
+        <td class="pr-6 py-2">
+          NC Dexter
+        </td>
+        <td class="px-12 py-2">
+          not out
+        </td>
+        <td class="pl-6 py-2 text-right">
+          13
+        </td>
+      </tr>
+      <tr>
+        <td class="pr-6 py-2">
+          DCI Endeavour
+        </td>
+        <td class="px-12 py-2">
+          not out
+        </td>
+        <td class="pl-6 py-2 text-right">
+          6
+        </td>
+      </tr>
+    </tbody>
   </table>
+  
   <div class="flex items-baseline justify-between my-8">
     <p class="text-gray-400">
       4.0 overs
@@ -35,6 +51,64 @@
       19 / 0
     </p>
   </div>
+
+  <table class="table-auto w-full my-8">
+    <thead>
+      <tr>
+        <th class="text-left  font-semibold">
+          Bowler
+        </th>
+        <th class="text-right w-12 font-semibold">
+          O
+        </th>
+        <th class="text-right w-12 font-semibold">
+          M
+        </th>
+        <th class="text-right w-12 font-semibold">
+          R
+        </th>
+        <th class="text-right w-12 font-semibold">
+          W
+        </th>
+      </tr>
+    </thead>
+    <tbody class="font-mono text-gray-400">
+      <tr>
+        <td class="py-2">
+          JE Thaw
+        </td>
+        <td class="text-right w-12">
+          2
+        </td>
+        <td class="text-right w-12">
+          0
+        </td>
+        <td class="text-right w-12">
+          9
+        </td>
+        <td class="text-right w-12">
+          0
+        </td>
+      </tr>
+      <tr>
+        <td class="py-2">
+          SF Evans
+        </td>
+        <td class="text-right w-12">
+          2
+        </td>
+        <td class="text-right w-12">
+          0
+        </td>
+        <td class="text-right w-12">
+          10
+        </td>
+        <td class="text-right w-12">
+          0
+        </td>
+      </tr>
+    </tbody>
+  </table>
   <h2 class="text-xl font-semibold mt-6">
     Live report
   </h2>
